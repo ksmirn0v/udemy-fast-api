ee7adc81375868824e658e8bf1a021469ef7b947
ksmirnov <kirill.smirnov@konux.de>
2023-08-13 13:18:01 +0200

    books-advanced: fetched a book by id

--- books/books_advanced.py
+++ books/books_advanced.py
@@ -54,6 +54,13 @@ def get_book():
     return BOOKS
 
 
+@app.get("/book/{book_id}")
+def get_book_by_id(book_id: int):
+    for book in BOOKS:
+        if book.id == book_id:
+            return book
+
+
 @app.post("/books/create_book")
 def create_book(book_request: BookRequest):
     book = Book(**book_request.model_dump())
