35ef9420bb50837ef2e1210f77515073d8ac70bd
ksmirnov <kirill.smirnov@konux.de>
2023-08-13 18:07:14 +0200

    todos: added 'add_record()' endpoint

--- todos/main.py
+++ todos/main.py
@@ -1,6 +1,7 @@
 from typing import Annotated
 
 from fastapi import FastAPI, Depends, HTTPException, Path
+from pydantic import BaseModel, Field
 from sqlalchemy.orm import Session
 from starlette import status
 
@@ -14,6 +15,14 @@ app = FastAPI()
 models.base.metadata.create_all(bind=engine)
 
 
+class TodosRequest(BaseModel):
+
+    title: str = Field(min_length=3)
+    description: str = Field(min_length=3, max_length=100)
+    priority: int = Field(gt=0, lt=6)
+    complete: bool
+
+
 def get_db():
 
     db = session()
@@ -23,7 +32,7 @@ def get_db():
         db.close()
 
 
-@app.get('/', status_code=status.HTTP_200_OK)
+@app.get('/todos', status_code=status.HTTP_200_OK)
 async def get_all_records(db: Annotated[Session, Depends(get_db)]):
     return db.query(models.Todos).all()
 
@@ -37,3 +46,10 @@ async def get_records_by_id(db: Annotated[Session, Depends(get_db)], record_id: 
         status_code=404,
         detail=f"The record with id={record_id} was not found in the database!"
     )
+
+
+@app.post('/todos/create-todo', status_code=status.HTTP_201_CREATED)
+async def add_record(db: Annotated[Session, Depends(get_db)], todos_request: TodosRequest):
+    todos_model = models.Todos(**todos_request.model_dump())
+    db.add(todos_model)
+    db.commit()
