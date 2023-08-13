9a035d864817926cc4a3b8f58d76db06bb3b06e8
ksmirnov <kirill.smirnov@konux.de>
2023-08-13 11:21:59 +0200

    books-advanced: prepared a new project

---
+++ books/books_advanced.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+
+class Book:
+
+    id: int
+    title: str
+    author: str
+    description: str
+    rating: int
+
+    def __init__(self, id: int, title: str, author: str, description: str, rating: int):
+        self.id = id
+        self.title = title
+        self.author = author
+        self.description = description
+        self.rating = rating
+
+
+BOOKS = [
+    Book(id=1, title="title 1", author="author 1", description="description 1", rating=5),
+    Book(id=2, title="title 2", author="author 2", description="description 2", rating=5),
+    Book(id=3, title="title 3", author="author 3", description="description 3", rating=1),
+]
+
+
+@app.get("/books")
+def get_book():
+    return BOOKS
