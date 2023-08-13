2a333883a4ee98e7cf8303abea576d63df6d02f4
ksmirnov <kirill.smirnov@konux.de>
2023-08-13 17:49:13 +0200

    todos: added 'get_all()' endpoint

--- todos/main.py
+++ todos/main.py
@@ -1,10 +1,27 @@
-from fastapi import FastAPI
+from typing import Annotated
+
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
 
 import models
-from database import engine
+from database import engine, session
 
 
 app = FastAPI()
 
 
 models.base.metadata.create_all(bind=engine)
+
+
+def get_db():
+
+    db = session()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.get('/')
+async def get_all_records(db: Annotated[Session, Depends(get_db)]):
+    return db.query(models.Todos).all()
