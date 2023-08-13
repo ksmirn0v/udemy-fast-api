d90a4e8c18c6e57a73e3107c7dd15696f02f183a
ksmirnov <kirill.smirnov@konux.de>
2023-08-13 11:48:43 +0200

    books-advanced: added field validatations

--- books/books_advanced.py
+++ books/books_advanced.py
@@ -1,5 +1,7 @@
+from typing import Optional
+
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 app = FastAPI()
@@ -23,11 +25,11 @@ class Book:
 
 class BookRequest(BaseModel):
 
-    id: int
-    title: str
-    author: str
-    description: str
-    rating: int
+    id: Optional[int] = None
+    title: str = Field(min_length=3)
+    author: str = Field(min_length=1)
+    description: str = Field(min_length=3, max_length=100)
+    rating: int = Field(gt=0, lt=6)
 
 
 BOOKS = [
@@ -45,4 +47,12 @@ def get_book():
 @app.post("/books/create_book")
 def create_book(book_request: BookRequest):
     book = Book(**book_request.model_dump())
+    create_id(book=book)
     BOOKS.append(book)
+
+
+def create_id(book: Book):
+    if len(BOOKS) > 0:
+        book.id = BOOKS[-1].id + 1
+    else:
+        book.id = 1
