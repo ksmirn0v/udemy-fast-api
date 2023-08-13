1c130a7751becb44e8b9b3412f0756e311253f28
ksmirnov <kirill.smirnov@konux.de>
2023-08-13 10:51:12 +0200

    books-3

--- books/books.py
+++ books/books.py
@@ -19,8 +19,29 @@ async def read_all_books():
     return BOOKS
 
 
-@app.get("/book/{book_title}")
+@app.get("/books/{book_title}")
 async def read_book_by_title(book_title: str):
     for book in BOOKS:
         if book.get('title').casefold() == book_title.casefold():
             return book
+
+
+@app.get("/books/")
+async def read_book_by_query(category: str):
+    books_to_return = []
+    for book in BOOKS:
+        if book.get('category').casefold() == category.casefold():
+            books_to_return.append(book)
+    return books_to_return
+
+
+@app.get("/books/{book_author}/")
+async def read_book_by_author_and_query(book_author: str, category: str):
+    books_to_return = []
+    for book in BOOKS:
+        if (
+            book.get('author').casefold() == book_author.casefold() and
+            book.get('category').casefold() == category.casefold()
+        ):
+            books_to_return.append(book)
+    return books_to_return
