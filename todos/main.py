aaa3712b7c38ea47b959cebcdb2d4a4581e4b300
ksmirnov <kirill.smirnov@konux.de>
2023-08-13 17:55:51 +0200

    todos: added 'get_records_by_id()' endpoint

--- todos/main.py
+++ todos/main.py
@@ -1,7 +1,8 @@
 from typing import Annotated
 
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException, Path
 from sqlalchemy.orm import Session
+from starlette import status
 
 import models
 from database import engine, session
@@ -22,6 +23,17 @@ def get_db():
         db.close()
 
 
-@app.get('/')
+@app.get('/', status_code=status.HTTP_200_OK)
 async def get_all_records(db: Annotated[Session, Depends(get_db)]):
     return db.query(models.Todos).all()
+
+
+@app.get('/todos/{record_id}', status_code=status.HTTP_200_OK)
+async def get_records_by_id(db: Annotated[Session, Depends(get_db)], record_id: int = Path(gt=0)):
+    output = db.query(models.Todos).filter(models.Todos.id == record_id).first()
+    if output is not None:
+        return output
+    raise HTTPException(
+        status_code=404,
+        detail=f"The record with id={record_id} was not found in the database!"
+    )
